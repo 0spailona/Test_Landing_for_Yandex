b7fb10805e0c89b800c4eff25978919dc51a3f29
Daria <n-d-p@mail.ru>
2024-11-01 00:54:37 +0300

    block with table

--- ya_test_landing/src/components/main/cards/cardWithTable/cardWithTable.jsx
+++ ya_test_landing/src/components/main/cards/cardWithTable/cardWithTable.jsx
@@ -1,16 +1,23 @@
 import "./css/cardWithTable.css";
+import "./css/cardMedia.css"
 import Image from "../../../../assets/cardWithTable.png";
 import MyTable from "./myTable.jsx";
+import MediaQuery from "react-responsive";
 
 export default function CardWithTable() {
+
     return (<div className="cardWithTable-content">
-        <div className="cardWithTable-img-wrp"><img src={Image} alt="img"/></div>
-        <div className="cardWithTable-info">
-            <div className="cardWithTable-title upperCase fontSerif"><span className="card-text-main">и Сеанс</span><span className="card-text-event"> одновременной игры в шахматы на 160 досках</span>
-                <span className="card-text-main"> гроссмейстера О. Бендера</span>
+            <div className="cardWithTable-img-wrp"><img src={Image} alt="img"/></div>
+            <div className="cardWithTable-info">
+                <div className="cardWithTable-title upperCase fontSerif"><span
+                    className="card-text-main">и Сеанс</span><span
+                    className="card-text-event"> одновременной игры в{"\u00A0"}шахматы на{"\u00A0"}160{"\u00A0"}досках</span>
+                    <span className="card-text-main"> гроссмейстера О.{"\u00A0"}Бендера</span>
+                </div>
+                <MyTable/>
+                <div className="cardWithTable-add-info fontSansSerif">По{"\u00A0"}всем вопросам обращаться в{"\u00A0"}администрацию
+                    к{"\u00A0"}К.{"\u00A0"}Михельсону
+                </div>
             </div>
-            <MyTable/>
-            <div className="cardWithTable-add-info"></div>
-        </div>
     </div>);
 }
--- ya_test_landing/src/components/main/cards/cardWithTable/myTable.jsx
+++ ya_test_landing/src/components/main/cards/cardWithTable/myTable.jsx
@@ -1,5 +1,27 @@
 import "./css/myTable.css"
+import "./css/tableMedia.css"
 
-export default function MyTable(){
-    return <table className="table"></table>
+export default function MyTable() {
+    return <div className="cardWithTable-table fontSansSerif">
+        <table>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
 }
+/*<div className="table-column column-data-name">
+            <div className="table-row table-row-name">Место проведения:</div>
+            <div className="table-row table-row-name">Дата и время мероприятия:</div>
+            <div className="table-row table-row-name">Стоимость входных билетов:</div>
+            <div className="table-row table-row-name">Плата за игру:</div>
+            <div className="table-row table-row-name">Взнос на телеграммы:</div>
+        </div>
+        <div className="table-column column-data-text">
+            <div className="table-row table-row-text">Клуб «Картонажник»</div>
+            <div className="table-row table-row-text">22 июня 1927 г. в 18:00</div>
+            <div className="table-row table-row-text">20 коп.</div>
+            <div className="table-row table-row-text">50 коп.</div>
+            <div className="table-row table-row-text"><span
+                className="text-line-through">100 руб.</span><span>{'\u00A0'}21 руб. 16 коп.</span></div>
+        </div>*/
