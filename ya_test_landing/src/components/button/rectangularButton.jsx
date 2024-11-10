cda8a15c51bdeb72831fe03bb15921fedf0709fc
Daria <n-d-p@mail.ru>
2024-11-11 01:48:40 +0300

    ++ ready

--- ya_test_landing/src/components/button/rectangularButton.jsx
+++ ya_test_landing/src/components/button/rectangularButton.jsx
@@ -2,7 +2,7 @@ import "./button.css";
 
 export default function RectangularButton({mainColor, text, event, size}) {
     return (
-        <button className={`${mainColor}Btn ${size} rectangular-btn btn fontSansSerif`} onClick={() => event.call()}>
+        <button className={`btn rectangular-btn ${size} fontSansSerif ${mainColor}Btn`} onClick={() => event.call()}>
             <div className="btn-text text font-w-medium text-align-center fontSansSerif">{text ? text : ""}</div>
         </button>
     );
